3552dd1d1d0d8255acac4e00fa0b45d1683fac9a
SirKaneloRex <57345797+SirKaneloRex@users.noreply.github.com>
2023-03-06 03:45:10 -0600

    Add files via upload

---
+++ FileBrowser/src/filebrowser/FileBrowser.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filebrowser;
+
+/**
+ *
+ * @author julia
+ */
+public class FileBrowser {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        VFileBrowser vv=new VFileBrowser();
+        vv.setVisible(true);
+    }
+    
+}
