3ee3f65d6335da349109d1085b31dbe50693deb2
SirKaneloRex <57345797+SirKaneloRex@users.noreply.github.com>
2023-06-12 04:55:13 -0600

    Add files via upload

---
+++ MiniEncuesta_sqlEsteSI/src/miniencuesta/MiniEncuesta.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miniencuesta;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author julia
+ */
+public class MiniEncuesta {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Ventana1 v1 =new Ventana1();
+        v1.setVisible(true);
+        v1.setResizable(false);
+        
+        
+        
+        
+    }
+    
+}
